68527adb2f0ec37acb324fc72e02077d293fb3e1
Daniel Jones <messyjones@gmail.com>
2022-05-17 00:55:31 -0400

    the background blur is better

--- scholarshipz/Store/StoreView.swift
+++ scholarshipz/Store/StoreView.swift
@@ -28,20 +28,21 @@ class StoreView: UIView {
             make.height.equalToSuperview().multipliedBy(0.25)
         }
         
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        bannerImgView.addSubview(nameLabel)
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(10)
+            make.bottom.equalToSuperview().inset(5)
+        }
+        
         let backgroundBlur = UIView()
         backgroundBlur.backgroundColor = UIColor.blackBackground.withAlphaComponent(0.3)
         bannerImgView.addSubview(backgroundBlur)
         backgroundBlur.snp.makeConstraints { make in
-            make.leading.trailing.bottom.equalToSuperview()
-            make.height.equalTo(98)
-        }
-        
-        nameLabel.textColor = .white
-        nameLabel.font = UIFont.systemFont(ofSize: 20, weight: .medium)
-        backgroundBlur.addSubview(nameLabel)
-        nameLabel.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(10)
-            make.bottom.equalToSuperview().inset(5)
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(nameLabel).offset(-5)
+            make.bottom.equalToSuperview()
         }
     }
 }
